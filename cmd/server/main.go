bbef57c446a4f669f36e0db0755f4dda416f9678
treeforest <1451670604@qq.com>
2021-01-18 17:05:06 +0800

    feat: add auth of user by jwt

--- cmd/server/main.go
+++ cmd/server/main.go
@@ -1,14 +1,37 @@
 package main
 
 import (
+	"context"
 	"flag"
 	"fmt"
 	"github.com/treeforest/grpc-pcbook/pb"
 	"github.com/treeforest/grpc-pcbook/service"
 	"google.golang.org/grpc"
+	"google.golang.org/grpc/reflection"
 	"log"
 	"net"
 )
+
+func unaryInterceptor(
+	ctx context.Context,
+	req interface{},
+	info *grpc.UnaryServerInfo,
+	handler grpc.UnaryHandler,
+	) (resp interface{}, err error) {
+	log.Println("--> unary interceptor: ", info.FullMethod)
+	return handler(ctx, req)
+}
+
+func streamInterceptor (
+	srv interface{},
+	stream grpc.ServerStream,
+	info *grpc.StreamServerInfo,
+	handler grpc.StreamHandler,
+	) error {
+	log.Println("--> stream interceptor: ", info.FullMethod)
+	return handler(srv, stream)
+}
+
 
 func main() {
 	port := flag.Int("port", 0, "the server port")
@@ -19,8 +42,13 @@ func main() {
 	imageStore := service.NewDiskImageStore("img")
 	ratingStore := service.NewInMemoryRatingStore()
 	laptopServer := service.NewLaptopServer(laptopStore, imageStore, ratingStore)
-	grpcServer := grpc.NewServer()
+
+	grpcServer := grpc.NewServer(
+		grpc.UnaryInterceptor(unaryInterceptor),
+		grpc.StreamInterceptor(streamInterceptor),
+		)
 	pb.RegisterLaptopServiceServer(grpcServer, laptopServer)
+	reflection.Register(grpcServer)
 
 	address := fmt.Sprintf("0.0.0.0:%d", *port)
 	listener, err := net.Listen("tcp", address)
--- go.mod
+++ go.mod
@@ -3,10 +3,12 @@ module github.com/treeforest/grpc-pcbook
 go 1.15
 
 require (
+	github.com/dgrijalva/jwt-go v3.2.0+incompatible
 	github.com/golang/protobuf v1.4.3
 	github.com/google/uuid v1.1.4
 	github.com/jinzhu/copier v0.2.0
 	github.com/stretchr/testify v1.6.1
+	golang.org/x/crypto v0.0.0-20190308221718-c2843e01d9a2
 	google.golang.org/grpc v1.27.0
 	google.golang.org/protobuf v1.25.0
 )
